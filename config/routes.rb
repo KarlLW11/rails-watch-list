29a1eeeb8dbf8c141a675517848fc08861ee691a
Karl Vandenabeele <karl.vandenabeele@gmail.com>
2023-06-15 16:52:18 +0200

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create]  do  #on veut pas de destroy
+    resources :bookmarks, only: [:create, :new]
+  end
+    # inserer bookmark routes pour create et new
+  resources :bookmarks, only: [:destroy] #only destroy
+  resources :lists, only: [:destroy]
 end
